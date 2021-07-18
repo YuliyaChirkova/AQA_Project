373d9f37fc67568f26ff1dfc6a07612648b74d08
Yuliya Chirkova <chirkovay2014@gmail.com>
2021-07-19 01:14:21 +0300

    finish commit

--- src/test/java/AddressBook.java
+++ src/test/java/AddressBook.java
@@ -5,24 +5,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
+import java.util.List;
 
-public class Task1_AddressBook {
+public class AddressBook {
 
     public static void main(String[] args) {
+
         System.setProperty("webdriver.chrome.driver", "C:\\Webdriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String url = "http://a.testaddressbook.com/sign_in";
-
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
         driver.get(url);
 
         //Login
+
         driver.findElement(By.id("session_email")).sendKeys("www@www.qqq");
         driver.findElement(By.name("session[password]")).sendKeys("123QWE");
         driver.findElement(By.cssSelector("input[value='Sign in']")).click();
 
-        // add address
+        // Add address
+
         driver.findElement(By.cssSelector("a[data-test='addresses']")).click();
         driver.findElement(By.cssSelector("a[data-test='create']")).click();
         driver.findElement(By.name("address[first_name]")).sendKeys("Rodger");
@@ -47,6 +49,31 @@ public class Task1_AddressBook {
         driver.findElement(By.xpath("//input[@id='address_interest_climb']")).click();
         driver.findElement(By.id("address_note")).sendKeys("home address");
         driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
+        driver.findElement(By.cssSelector("a[data-test='addresses']")).click();
+
+    // Edit address
+
+        driver.findElement(By.xpath("//a[text()='Edit'][1]")).click();
+        driver.findElement(By.id("address_city")).clear();
+        driver.findElement(By.id("address_city")).sendKeys("New York");
+        Select drpCountry2 = new Select(driver.findElement(By.name("address[state]")));
+        drpCountry2.selectByVisibleText("New York");
+        driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
+        driver.findElement(By.cssSelector("a[data-test='addresses']")).click();
+
+   // Delete address
+
+        List<WebElement> listOfElements = driver.findElements(By.xpath("//a[text()='Destroy']"));
+
+       if(listOfElements.size()>=1){
+            listOfElements.get(0).click();
+        }else{
+            System.out.println("There are no addresses in the AddressBook");
+        }
+        driver.switchTo().alert().accept();
+
+       //Sign out
+        driver.findElement(By.xpath("//a[text()='Sign out']")).click();
 
 
         driver.close();
