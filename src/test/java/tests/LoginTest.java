75cd2282dcce4b486da209f292bcc9493a60a697
Yuliya Chirkova <chirkovay2014@gmail.com>
2021-07-28 00:12:29 +0300

    non final commit

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -19,9 +19,9 @@ public class LoginTest extends BeforeAll_AfterAll{
     @Tag("SkipClick")
     @DisplayName("Testing of Login page")
     public void Login() {
-        driver.findElement(By.id("session_email")).sendKeys("www@www.qqq");
+        /*driver.findElement(By.id("session_email")).sendKeys("www@www.qqq");
         driver.findElement(By.name("session[password]")).sendKeys("123QWE");
-        driver.findElement(By.cssSelector("input[value='Sign in']")).click();
+        driver.findElement(By.cssSelector("input[value='Sign in']")).click();*/
         getLogin();
         String title = driver.getTitle();
         Assertions.assertEquals("Address Book", title, "The incorrect page is open or invalid title is specified");
