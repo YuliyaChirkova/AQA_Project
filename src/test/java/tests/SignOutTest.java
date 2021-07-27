b9def35d3476fef076d36f775905c27e4a051309
Yuliya Chirkova <chirkovay2014@gmail.com>
2021-07-28 00:27:34 +0300

    non final commit

--- src/test/java/tests/SignOutTest.java
+++ src/test/java/tests/SignOutTest.java
@@ -1,17 +1,14 @@
 package tests;
-
-import com.sun.deploy.cache.BaseLocalApplicationProperties;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import utils.Driver;
 
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SignOutTest extends BeforeAll_AfterAll{
 
 
-  /* @Test
+   @Test
    @Order(1)
    public void Login() {
        driver.findElement(By.id("session_email")).sendKeys("www@www.qqq");
@@ -19,13 +16,13 @@ public class SignOutTest extends BeforeAll_AfterAll{
        driver.findElement(By.cssSelector("input[value='Sign in']")).click();
        String title = driver.getTitle();
        Assertions.assertEquals("Address Book", title, "The incorrect page is open or invalid title is specified");
-   }*/
+   }
 
     @Test
-   // @Order(2)
+    @Order(2)
     void SignOut() {
-        LoginTest login = new LoginTest();
-        login.getLogin();
+       /* LoginTest login = new LoginTest();
+        login.getLogin();*/
         driver.findElement(By.xpath("//a[text()='Sign out']")).click();
         String currentURL = driver.getCurrentUrl();
         Assertions.assertEquals("http://a.testaddressbook.com/sign_in", currentURL, "The incorrect page is open or invalid URL is specified");
